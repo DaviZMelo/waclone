3bea23eb9ece5ff3d6d95b105e08d602c0eb1d6a
Davi Melo <extremexgamer32@gmail.com>
2021-06-30 20:06:43 -0300

    refactor: remove update method

--- src/modules/admin/infra/http/controllers/GroupsController.ts
+++ src/modules/admin/infra/http/controllers/GroupsController.ts
@@ -1,30 +1,18 @@
 import ListGroupsIamAdminService from '@modules/admin/services/ListGroupsService';
-import SetGroupsService from '@modules/admin/services/SetGroupsService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 export default class GroupsController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const isAdmin = Boolean(Number(request.query.admin_groups));
+    const { admin_groups } = request.query;
     const listGroupsIamAdminService = container.resolve(
       ListGroupsIamAdminService,
     );
 
-    const groupsIamAdmin = await listGroupsIamAdminService.execute(isAdmin);
+    const groupsIamAdmin = await listGroupsIamAdminService.execute(
+      admin_groups === 'true',
+    );
 
     return response.json(groupsIamAdmin);
-  }
-
-  public async update(request: Request, response: Response): Promise<Response> {
-    const { hostGroupId, targetGroupId, targetGroupLink } = request.body;
-    const setGroupsService = container.resolve(SetGroupsService);
-
-    await setGroupsService.execute({
-      hostGroupId,
-      targetGroupId,
-      targetGroupLink,
-    });
-
-    return response.status(202).json();
   }
 }
